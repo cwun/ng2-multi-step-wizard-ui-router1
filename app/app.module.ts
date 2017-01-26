2b581ac0bc0037cbd296317a3c939f40168a1171
Cathy Wun <cathyhwun@gmail.com>
2017-01-26 17:39:42 -0600

    Added loading indicator

--- app/app.module.ts
+++ app/app.module.ts
@@ -14,7 +14,7 @@ import { AddressComponent }   from './address/address.component';
 import { ResultComponent }    from './result/result.component';
 
 /* App Router */
-import { UIRouterConfigFn }     from "./app.router";
+import { UIRouterConfigFn }   from "./app.router";
 import { appStates }          from "./app.states";
 
 /* Shared Service */
